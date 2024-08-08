295f355529abda7b381b318c1eeb763588f61f23
ArtsiomArchiptsov <158454533+ArtsiomArchiptsov@users.noreply.github.com>
2024-08-08 18:27:51 +0300

    Главный цикл программы

--- main.java
+++ main.java
@@ -47,12 +47,12 @@ public class main {
             switch (choice) {
                 case 1:
                     System.out.println("Введите мин ОЗУ(GB): ");
-                    int minRam = scanner.nextInt();//тут не проверяется на число и если ввести буквы вылетает
+                    int minRam = scanner.nextInt();
                     filters.put(1, minRam);
                     break;
                 case 2:
                     System.out.println("Введите мин HDD(GB): ");
-                    int minStorage = scanner.nextInt();//тут не проверяется на число и если ввести буквы вылетает
+                    int minStorage = scanner.nextInt();
                     filters.put(2, minStorage);
                     break;
                 case 3:
